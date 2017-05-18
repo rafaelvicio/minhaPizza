babb57823e86c06a1dcbc024e9aeb2a4fd6bf38b
Rafael Augusto <rafaelvicio@icloud.com>
2017-05-18 17:05:00 -0300

    Projeto base

---
+++ js/controller.js
@@ -0,0 +1,35 @@
+angular.module("minhaPizza")
+    .controller("novoSaborCtrl", ['$scope', '$routeParams', function($scope, $routeParams){
+
+    $scope.pizza = {
+      nome: $routeParams.nome,
+      formato: "Redonda",
+      local: "Local",
+      endereco: "",
+      ingredientes: [],
+      total: 0
+    };
+
+    $scope.Ingrediente = {
+      novo: "",
+      inserir: function(){
+        $scope.pizza.ingredientes.push(this.novo);
+        this.novo = "";
+      },
+      remover: function(indice){
+        $scope.pizza.ingredientes.splice(indice, 1);
+      }
+    };
+
+    $scope.formatoPizza = function(){
+      return 'pizza-formato' + ($scope.pizza.formato === "Redonda" ? "pizza-redonda" : "pizza-quadrada");
+    };
+
+    $scope.precoPizza = function(){
+      var total = $scope.pizza.ingredientes.length * 1.5;
+      total += ($scope.pizza.formato === 'Redonda' ? 2 : 5);
+      total += ($scope.pizza.formato === 'Quadrada' ? 5 : 0);
+      return total;
+    };
+
+  }]);
